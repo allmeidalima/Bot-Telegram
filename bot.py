c8434404421692251797197b38fecd771fba8297
unknown <Lucas>
2022-02-03 20:38:46 -0300

    Desenvolvimento de funções para interação com as respostas dos usuários

--- bot.py
+++ bot.py
@@ -3,15 +3,27 @@ import json
 class Wolf():
 
     def __init__(self):
-        self.memory = open('memoria.json', 'w')
-        self.memory.write('''[["WOLF", "Lucas", "Claudia"], {"Bom dia" : "Bom dia", "Boa tarde" : "Boa tarde", "Boa noite" : "Boa noite"}]''')
-        self.memory.close()
-        self.memory = open('memoria.json', 'r')
+        try:
+            self.memory = open('memoria.json', 'r')
+        except FileNotFoundError:
+            self.memory = open('memoria.json', 'w')
+            self.memory.write('''[["WOLF", "Lucas", "Claudia"], {"Bom dia" : "Bom dia", "Boa tarde" : "Boa tarde", "Boa noite" : "Boa noite", "Como você esta?" : "Eu estou bem, obrigado por perguntar."}]''')
+            self.memory.close()
+            self.memory = open('memoria.json', 'r')
 
         self.people, self.message = json.load(self.memory)
         self.memory.close()
+        self.historic = [None]
 
+    def listen(self, phrase=None):
+        return phrase
 
-Wolf()
+    def think(self, phrase):
+        if phrase in self.message:
+            return self.message[phrase]
+
+    def speak(self, bot_phrase):
+        self.historic.append(bot_phrase)
+#Wolf()
 
 
--- main.py
+++ main.py
@@ -2,23 +2,29 @@ import time, telepot, json
 from telepot.loop import MessageLoop
 from bot import Wolf
 
+with open('token.json', 'r') as file:
+    TOKEN = json.load(file)
+
+telegram = telepot.Bot(TOKEN)
+bot = Wolf()
+
 def handle(msg):
+    user_phrase = bot.listen(phrase=msg['text'])
+    response = bot.think(user_phrase)
+    bot.speak(response)
     content_type, chat_type, chat_id = telepot.glance(msg)
+    telegram.sendMessage(chat_id, response)
+    
     #print(msg)
     #print(content_type, chat_type, chat_id)
     
+    #if content_type == 'text':
 
-    if content_type == 'text':
-        bot.sendMessage(chat_id, " ")
-
-with open('token.json', 'r') as file:
-    TOKEN = json.load(file) 
+MessageLoop(telegram, handle).run_as_thread()
+print ('Listening ...')
 
 #TOKEN = json_file
 
-bot = telepot.Bot(TOKEN)
-MessageLoop(bot, handle).run_as_thread()
-print ('Listening ...')
 
 # Keep the program running.
 while 1:
